e4b2393cf22f05f5d35cb40ed6a370dd69e9f3a6
abdulsattar <abdulrahman86@gmail.com>
2020-05-28 07:26:17 -0400

    Scrolling support

--- testos/kernel/kernel.c
+++ testos/kernel/kernel.c
@@ -1,8 +1,17 @@
+#include "../drivers/screen.h"
+#include "util.h"
 
 void main() {
     clear_screen();
-    kprint_at("X", 1, 6);
-    kprint_at("This text spans multiple lines", 75, 10);
-    kprint_at("There is a line\nbreak", 0, 20);
-    kprint("There is a line\nbreak");
+   
+    /* Fill up the screen */
+    int i = 0;
+    for (i = 0; i < 24; i++) {
+        char str[255];
+        int_to_ascii(i, str);
+        kprint_at(str, 0, i);
+    }
+
+    kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ", 60, 24);
+    kprint("And with this text, the kernel will scroll again, and row 1 will disappear too!");
 }
